46d0ca46f8a6b9fe002fb0885f144b4a6d85bcb1
RivalShyMex <kocsisregobotond@gmail.com>
2023-04-29 20:41:04 +0200

    Added more stuff

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 let number;
-let planet01 = document.getElementById("planet1");
-let planet02 = document.getElementById("planet2");
-let planet03 = document.getElementById("planet3");
-let planet04 = document.getElementById("planet4");
-let planet05 = document.getElementById("planet5");
-let planet06 = document.getElementById("planet6");
-let planet07 = document.getElementById("planet7");
-let planet08 = document.getElementById("planet8");
-let planet09 = document.getElementById("planet9");
-let planet10 = document.getElementById("planet10");
-let planet11 = document.getElementById("planet11");
-let planet12 = document.getElementById("planet12");
-let planet13 = document.getElementById("planet13");
-let planet14 = document.getElementById("planet14");
-let planet15 = document.getElementById("planet15");
-let planet16 = document.getElementById("planet16");
-let planet17 = document.getElementById("planet17");
-let planet18 = document.getElementById("planet18");
-let planet19 = document.getElementById("planet19");
-let planet20 = document.getElementById("planet20");
-
+let planetId;
+let planet;
 
 function RollDice() {
     number = Math.floor(Math.random() * 20 + 1);
+    planetId = `planet${number}`;
+    planet = document.getElementById(planetId);
+    let input = document.getElementById("planet1");
+    let rect = input.getBoundingClientRect();
 
-    if (number === parseInt(planet01.id.slice(-1))) {
-        document.getElementById('planet1').style.display = "block"
-        return number;
+    for (let index = 0; index <= 20; index++) {
+        console.log(number);
+
+        if (planet !== null) {
+            if (number === parseInt(planet.id.slice(-1))) {
+                if (itemUnlocked === "true") {
+                    localStorage.setItem("itemUnlocked", true);
+                    console.log(`Unlocked planet number ${number}`)
+                }
+                
+                planet.style.display = "block"
+                let itemUnlocked = localStorage.getItem("itemUnlocked");
+                let url1 = "planet1.html?itemUnlocked=true";
+                let urlParams = new URLSearchParams(window.location.search);
+                itemUnlocked = urlParams.get("itemUnlocked");
+                localStorage.setItem("ItemUnlocked", true);
+                window.location.href = url1;
+              
+                return number;
+            }
+        }
     }
-
-    return number;
-
 }
 
