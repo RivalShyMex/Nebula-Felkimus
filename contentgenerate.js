5ba90b9d9813a96500ac63af7183041d7cb80fd0
RivalShyMex <kocsisregobotond@gmail.com>
2023-05-05 12:15:06 +0200

    aaaaaaaaaaaaa

---
+++ contentgenerate.js
@@ -0,0 +1,102 @@
+let ergstring = "";
+let counter = 0;
+
+function generateCards() {
+    for (let i = 0; i < 6; i++) {
+        ergstring += `<div class= column>`
+        for (let j = 0; j < 3; j++) {
+            ergstring += `<div class="card">
+                <div class="name">
+                <p>${digimons[counter].name}</p>
+                </div>
+                <div class="infos">
+                    <div id="picture">
+                    
+                     <img src="./img/${digimons[counter].picture}">
+                      
+                    </div>
+                    <div class="texts">
+                      <div class="data">
+                        <div>
+                        <p>Level: </p> 
+                            <p>${digimons[counter].level}</p>    
+                        </div>
+                        <div>
+                            <p>Type: </p> 
+                            <p>${digimons[counter].type}</p>
+                        </div>
+                        <div>
+                            <p>Attribute: </p>
+                            <p>${digimons[counter].attribute}</p>
+                        </div>
+                      </div>   
+                     <div class="specialMoves">
+                        <p>Special moves:</p>
+                        <p>${digimons[counter].specialMove}</p>
+                     </div>
+                     <div class="profile">
+                        <p>Profile: ${digimons[counter].profile}</p>
+                     </div>
+                     </div>
+                    </div>
+                </div>`
+            counter++
+        }
+        ergstring += `</div>`
+    }
+    document.getElementById('contentgenerate').innerHTML = ergstring
+}
+
+function filterCards() {
+    counter = 0;
+    let filter = document.getElementById('attributeFilter').value;
+    if(filter == "all"){
+        ergstring = "";
+        generateCards();
+    }else{
+        ergstring = "";
+        ergstring += `<div class= column>`
+        for (let index = 0; index < digimons.length; index++) {
+            if(filter == digimons[index].attribute){
+                        ergstring += `<div class="card">
+                            <div class="name">
+                            <p>${digimons[index].name}</p>
+                            </div>
+                            <div class="infos">
+                                <div id="picture">
+                                
+                                 <img src="./img/${digimons[index].picture}">
+                                  
+                                </div>
+                                <div class="texts">
+                                  <div class="data">
+                                    <div>
+                                    <p>Level: </p> 
+                                        <p>${digimons[index].level}</p>    
+                                    </div>
+                                    <div>
+                                        <p>Type: </p> 
+                                        <p>${digimons[index].type}</p>
+                                    </div>
+                                    <div>
+                                        <p>Attribute: </p>
+                                        <p>${digimons[index].attribute}</p>
+                                    </div>
+                                  </div>   
+                                 <div class="specialMoves">
+                                    <p>Special moves:</p>
+                                    <p>${digimons[index].specialMove}</p>
+                                 </div>
+                                 <div class="profile">
+                                    <p>Profile: ${digimons[index].profile}</p>
+                                 </div>
+                                 </div>
+                                </div>
+                            </div>`
+                        counter++
+            }
+        }
+        ergstring += `</div>`
+        document.getElementById('contentgenerate').innerHTML = ergstring
+    }
+}
---
+++ listofcontent.js
@@ -0,0 +1,183 @@
+let digimon1 = {
+    name: "Gammamon",
+    level: "Rookie",
+    type: "Ceratopsian",
+    attribute: "Virus",
+    specialMove: "Horn Attack, Breaclaw",
+    profile: "An extremely rare, young white Ceratopsian Digimon that was recently discovered. Gammamon’s Digivolution is said to be related to a digital signal that came from somewhere outside our galaxy.",
+    picture: "gammamon.jpg"
+}
+
+let digimon2 = {
+    name: "Gallantmon",
+    level: "Mega",
+    type: "Holy Knight",
+    attribute: "Virus",
+    specialMove: "Lighting Joust, Shield of the Just",
+    profile: "Gallantmon is a Holy Knight Digimon counted among the Royal Knights alongside Omegamon and Magnamon. The Royal Knights are the highest-ranking Digimon when it comes to Network security, and no Digimon has ever been able to crack their stalwart defenses.",
+    picture: "dukemon.jpg"
+}
+
+let digimon3 = {
+    name: "Spadamon",
+    level: "Rookie",
+    type: "Weapon",
+    attribute: "Free",
+    specialMove: "Sugar Pound, Blade Blender, Aurora Blazer",
+    profile: "A Weapon Digimon that can transform its body into a weapon, and one of the fabled Legend-Arms said to be capable of either saving the world in the hands of an angel, or destroying it in the hands of a devil. Despite Spadamon’s serious and hardworking personality, it can also succumb to pressure from time to time.",
+    picture: "spadamon.jpg"
+}
+
+let digimon4 = {
+    name: "Missimon",
+    level: "In-Trainig II",
+    type: "Machine",
+    attribute: "None",
+    specialMove: "Missile Crash",
+    profile: "A Machine Digimon with a missile body. Its missile is ignited the moment it Digivolves into Missimon, and it continues to fly until it Digivolves to another form. It can adjust its speed somewhat, but will crash unless it maintains considerable speed, so it cannot stop in one place.",
+    picture: "missimon.jpg"
+}
+
+let digimon5 = {
+    name: "RizeGreymon",
+    level: "Ultimate",
+    type: "Android",
+    attribute: "Vaccine",
+    specialMove: "Trident Revolver, Rising Destroyer, Solid Strike",
+    profile: "An Android Digimon with a mechanized upper body. Despite RizeGreymon’s massive frame, it can still take to the skies to defeat foes.",
+    picture: "rizegreymon.jpg"
+}
+
+let digimon6 = {
+    name: "Beetlemon",
+    level: "Hybrid",
+    type: "Android",
+    attribute: "Variable",
+    specialMove: "Thor's Hammer, Mjölnir Thunder",
+    profile: "A Digimon with power over thunder that bears the might of the Ten Legendary Warriors. Beetlemon uses its movements, which are strangely nimble in spite of its size, to generate a high-voltage current that then courses through its body.",
+    picture: "blitzmon.jpg"
+}
+
+let digimon7 = {
+    name: "Shoutmon X4",
+    level: "Champion (Xros Wars)",
+    type: "Composite",
+    attribute: "Data",
+    specialMove: "Burning Star Crusher, Three Victorize, Three Impact, High Atmospheric Pressure, Varooma Boomerang",
+    profile: "A miraculous form known as Victory Mode is born from the strong desire of four Digimon to achieve absolute victory. It exists only to carry out this conviction.",
+    picture: "shoutmonx4.jpg"
+}
+
+let digimon8 = {
+    name: "Dracomon (X Antibody)",
+    level: "Rookie",
+    type: "Dragon",
+    attribute: "Data",
+    specialMove: "Baby Blaze, Smash Horn, Great Strike",
+    profile: "Dracomon is a Dragon Digimon of an old bloodline said to trace back to the progenitor of all Dramon Digimon. Dracomon (X Antibody)’s wings have developed further, allowing it to fly for short periods.",
+    picture: "dracomon_x.jpg"
+}
+
+let digimon9 = {
+    name: "Digmon",
+    level: "Armor",
+    type: "Insectoid",
+    attribute: "Free",
+    specialMove: "Big Crack, Gold Rush",
+    profile: "An Armor Insectoid Digimon that has Digivolved with the Digi-Egg of Knowledge. The Digi-Egg of Knowledge has the attribute of earth, allowing those who equip it to control the ground.",
+    picture: "digmon.jpg"
+}
+
+let digimon10 = {
+    name: "Machinedramon",
+    level: "Mega",
+    type: "Machine",
+    attribute: "Virus",
+    specialMove: "Infinity Cannon",
+    profile: "The strongest Digimon in the Digital World, with a 100% metal body. Since Machinedramon consists of a combination of parts from countless other cyborg Digimon, all of the other cyborg Digimon produced so far are thought to have been prototypes made so Machinedramon could be completed.",
+    picture: "mugendramon.jpg"
+}
+
+let digimon11 = {
+    name: "SkullKnightmon: Cavalier Mode",
+    level: "Champion (Xros Wars)",
+    type: "Enhancement",
+    attribute: "Virus",
+    specialMove: "Cavalier Braver",
+    profile: "SkullKnightmon: Cavalier Mode is an Assault Strike Mode where SkullKnightmon is mounted on DeadlyAxemon. Its primary purpose is to close in on foes by running towards them like a bullet, heedless of any obstacles that might be in the way.",
+    picture: "skullknightmoncavaliermode.jpg"
+}
+
+let digimon12 = {
+    name: "MachGaogamon",
+    level: "Ultimate",
+    type: "Android",
+    attribute: "Data",
+    specialMove: "Gaoga Tornado, Winning Knuckle, Howling Cannon",
+    profile: "MachGaogamon is an Android Digimon with an immensely powerful rocket engine on its back. While it never stays in the air long, it excels at hit-and-run combat where it can make full use of its propulsive bursts of speed.",
+    picture: "machgaogamon.jpg"
+}
+
+let digimon13 = {
+    name: "Aldamon",
+    level: "Hybrid",
+    type: "Wizard",
+    attribute: "Variable",
+    specialMove: "Brahmashira, Atomic Inferno",
+    profile: "A Digimon whose power over fire exceeds that of legend due to the unknown powers it obtained by fully inheriting the might of the Ten Legendary Warriors. Aldamon is a Demon God who transforms fear and anger into a righteous power to fight.",
+    picture: "aldamon.jpg"
+}
+
+let digimon14 = {
+    name: "Bukamon",
+    level: "In-Training II",
+    type: "Lesser",
+    attribute: "None",
+    specialMove: "Air Bubbles",
+    profile: "Despite resembling an aquatic dinosaur infant, Bukamon is a playful Digimon that can move as skillfully as a seahorse. However, the friendliness of Pichimon seems to have vanished, and Bukamon is quick to flee when others draw near.",
+    picture: "pukamon.jpg"
+}
+
+let digimon15 = {
+    name: "Mantaraymon (X Antibody)",
+    level: "Champion",
+    type: "Aquatic",
+    attribute: "Vaccine",
+    specialMove: "Paralyze Tail, Paralyze Bomb",
+    profile: "An Armor Aquatic Digimon that has Digivolved with the Digi-Egg of Reliability. Mantaraymon’s form resembles that of an aggressive speed-type Digimon, but it is a calm creature that swims leisurely through the wide Net Ocean.",
+    picture: "mantaraymon_x.jpg"
+}
+
+let digimon16 = {
+    name: "Filmon",
+    level: "Champion",
+    type: "Beast Man",
+    attribute: "Data",
+    specialMove: "Lighting Stinger, Crimson Slash",
+    profile: "A Beast Man Digimon with countless large quills growing from its body. Filmon’s red claws can pierce through anything, and its yellow quills pour energy into whatever they stab.",
+    picture: "filmon.jpg"
+}
+
+let digimon17 = {
+    name: "Magnamon",
+    level: "Armor",
+    type: "Holy Knight",
+    attribute: "Free",
+    specialMove: "Plasma Shot, Shining Gold Solar Storm, Magna Explosion",
+    profile: "An Armor Holy Knight Digimon that has Digivolved through the Digi-Egg of Miracles. The Digi-Egg of Miracles has the attribute of metal, and is made of the super metal Chrome Digizoid.",
+    picture: "magnamon.jpg"
+}
+
+let digimon18 = {
+    name: "Exermon",
+    level: "Champion",
+    type: "Insectoid",
+    attribute: "Data",
+    specialMove: "Insec-Mind, Defib Shock",
+    profile: "A Digimon that Digivolved from data on yoga teachings. Exermon lives primarily in alpine regions, where it practices its own unique fighting style that makes use of its peculiar invertebrate-like movements.",
+    picture: "exermon.jpg"
+}
+
+let digimons=[
+    digimon1,digimon2,digimon3,digimon4,digimon5,digimon6,digimon7,digimon8,digimon9,digimon10,digimon11,digimon12,digimon13,digimon14,digimon15,digimon16,digimon17,digimon18
+]
