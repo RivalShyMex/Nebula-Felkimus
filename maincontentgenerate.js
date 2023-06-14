70222d7781c5c25485e74156b613c7a584270e6a
RivalShyMex <kocsisregobotond@gmail.com>
2023-06-14 15:16:59 +0200

    aa

--- maincontentgenerate.js
+++ maincontentgenerate.js
@@ -13,9 +13,15 @@ function generateCards() {
                     <div class="allcontent${planetIndex}">
                     <div class="header">
                     <img src="../images/${planets[planetIndex].logo}" alt="logo">
+                    <p>${planets[planetIndex].developedby}</p>
+                    <p>${planets[planetIndex].describedbyme}</p>
                     </div>
                     <div class="maincontent">
                     <p>${planets[planetIndex].description}</p>
+                    <p>${planets[planetIndex].shortpartofplot}</p>
+                    <p>${planets[planetIndex].review1}</p>
+                    <p>${planets[planetIndex].review2}</p>
+                    <p>${planets[planetIndex].review3}</p>
                     <iframe width="560" height="315" src="${planets[planetIndex].trailer}" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     </div>
                     </div>
