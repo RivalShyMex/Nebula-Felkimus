64be2063ceb5f98b0709bf6f5c3a3a1b6eab8bf5
= <=>
2023-06-16 06:01:35 +0200

    aa

--- characterscontentgenerate.js
+++ characterscontentgenerate.js
@@ -9,15 +9,23 @@ function generateCards() {
   console.log(characters[planetIndex])
   console.log(characters[planetIndex].number)
   ergstring += `
+            <div id="charborder${planetIndex}>
+            <div class="charborder${planetIndex}>
             <div class="charbackgroundContent${planetIndex}"> 
-            <div class="charallcontent${planetIndex}">
-            <div class="charheader">
-            <img src="../images/${characters[planetIndex].picture}">
+            <div class="charallcontent${planetIndex}"">
+            <div class="charheader${planetIndex}">
+            <img src="../images/${characters[planetIndex].picture}" class="charpictures${planetIndex}">
+            <h1>${characters[planetIndex].name}</h1>
             <p>${characters[planetIndex].quote}</p>
             </div>
-            <div class="charcontent">
+            <div class="charcontent${planetIndex}">
             <p>${characters[planetIndex].description}</p>
             <p>${characters[planetIndex].reasonwhyIlikechar}</p>
+            </div>
+            <div class="charlinks${planetIndex}">
+            <a href="${characters[planetIndex].songthatfitsthem}" target="_onblank"><p>${characters[planetIndex].songthatfitsthemtext}</p></a>
+            </div>
+            </div>
             </div>
             </div>
             </div>
--- listofcharacterscontent.js
+++ listofcharacterscontent.js
@@ -1,160 +1,220 @@
 let matoro = {
     picture: "bnmatoro.png",
+    name: "Matoro",
     description: "Matoro was a Ko-Matoran of Metru Nui. He served as Turaga Nuju's interpreter on Mata Nui. Later, he became a Toa of Ice and one of the Toa Inika and later, Toa Mahri. He was one of the destined wearers of the Ignika, and ultimately gave his life to resurrect the Great Spirit.",
     quote: "“It ends here. One thousand years of fear and violence and darkness even in the daylight...all over now.”",
     reasonwhyIlikechar: "He was first just your average villager and then he became the biggest hero in the Bionicle universe. He has my respect and my love.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=AEg3ebsHYjQ",
+    songthatfitsthemtext: "Move Along",
     number: "0"
 }
 
 let acrid = {
     picture: "roracrid.png",
+    name: "Acrid",
     description: "A macabre, poison-spewing ex-prisoner, Acrid fights with a potent array of damage-over-time effects and self-sustaining melee ability, as well as a mix of long-range and crowd control options for any situation. It also sports the highest base damage stat of any Survivor barring the Heretic, allowing for even greater damage potential.",
     quote: "“..and so it left, with delusions of freedom.”",
     reasonwhyIlikechar: "He is silly looking space doggo with a fun loadout to play with.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=RffBrCLOKv0",
+    songthatfitsthemtext: "A Glacier Eventually Farts (And Don't You Listen to the Song of Life)",
     number: "1"
 }
 
 let theprisoner = {
     picture: "owprisoner.png",
+    name: "The prisoner",
     description: "The Prisoner was an inhabitant of the Stranger who is locked in the Sealed Vault.",
     quote: "“I hadn’t dared to hope that anyone would hear the signal, for how briefly I set it free…”",
     reasonwhyIlikechar: "The prisoner in my opinion has the saddest fate in the whole DLC of the game, but I really can't stop loving them, they look so cool.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=eu5fXqoyxuw",
+    songthatfitsthemtext: "A Dream of Home",
     number: "2"
 }
 
 let endymion = {
-    picture: "einhaender.png",
+    picture: "eheinhaender.png",
+    name: "Endymion",
     description: "The Endymion Mk. III is a simpler ship, equipped with dual standard machine guns, and room for only one gunpod. The machine guns are not more powerful, but the twin bullets have a larger hit area, making it easier to hit various targets.",
-    quote: "“ZOOOOOOOOOMMMMM”",
+    quote: "“...”",
     reasonwhyIlikechar: "This isn't really a character but more than the ship the player can choose and I like the way it handles, also it's designed is sick.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=Ej3-23tqvfA",
+    songthatfitsthemtext: "Badlands",
     number: "3"
 }
 
 let gabriel = {
     picture: "ukgabriel.png",
+    name: "Gabriel",
     description: "Gabriel is the main antagonist of ULTRAKILL and an archangel who carries out the will of the Council with a ruthless efficiency that brings him both fear and respect. After striking down King Minos and gaining the title Judge of Hell, as well as killing King Sisyphus and ending his insurrection, he sought to drive the Machines out from its depths. When he was defeated by V1 in Gluttony, the Council believed him to be squandering his power, and cut him off from the light of The Father. Unsurprisingly, this did not go well for them as they were all slain by Gabriel after he was beaten by the machine a second time in Heresy.",
     quote: "“You Insignificant FUCK!”",
     reasonwhyIlikechar: "Gabriel has a really great and well written development and overall is well written.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=AnLuKx_c7Ds",
+    songthatfitsthemtext: "The Death of God's Will",
     number: "4"
 }
 
 let necrons = {
     picture: "w40knecrons.png",
+    name: "Necrons",
     description: "The Necrons are a mysterious xenos species of humanoid, robotic skeletal warriors that have lain dormant in their stasis-tombs for more than 60 million Terran years, though they have begun to awaken at last. They are the soulless creations and former servants of the ancient C'tan, the terrible Star Gods of Aeldari myth.",
     quote: "“There is a terrible darkness descending upon the galaxy, and we shall not see it ended in our lifetimes.”",
     reasonwhyIlikechar: "The necrons are basically skeletons but at the same time robots. I love that type of shit.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=mcGR3rSFkQY",
+    songthatfitsthemtext: "The Transference",
     number: "5"
 }
 
 let missioncontrol = {
     picture: "drgmissioncontrol.png",
+    name: "Mission control",
     description: "Mission Control is the Deep Rock Galactic corporate representative who oversees the employees of Deep Rock Galactic as they go about their business, either on the Space Rig or in Missions. He is responsible for a variety of intelligence-related tasks to keep players aware of any needed information, such as informing players about their objectives during a mission, or of an incoming swarm.",
     quote: "“You boys better be better miners than you are dancers.”",
     reasonwhyIlikechar: "The mission control has some of the best comments on what the players are doing, I always love to hear his voice lines when they come up.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=RmRRfd2-VeM",
+    songthatfitsthemtext: "RUN!",
     number: "6"
 }
 
 let isaac = {
     picture: "dsisaac.png",
+    name: "Isaac Clark",
     description: "Isaac Clarke was a former ship systems engineer who worked for the Concordance Extraction Corporation, and the protagonist of the Dead Space series.",
     quote: "“Stick around. I'm full of bad ideas.”",
     reasonwhyIlikechar: "Isaac is one the most badass protagonist in any series, I mean he has some trauma but who doesn't?",
+    songthatfitsthem: "https://www.youtube.com/watch?v=Y6Kz6aXsBSs",
+    songthatfitsthemtext: "Black Hole Sun",
     number: "7"
 }
 
 let gammamon = {
     picture: "dmgammamon.png",
+    name: "Gammamon",
     description: "Gammamon is a Ceratopsian Digimon. It is a very unusual, young, white Digimon that was recently discovered. Digivolution to Gammamon is said to be linked to 'digital signals that came from extragalactic space'. The two hard horns that grow from its brow are useful weapons for offense and defense. It is also capable of flying for a short period with the tiny wings on its back, making its body float. It rarely shows its emotions, but it seems that once you reach out to it, it will gradually become attached to you. It is a Rookie Digimon with three horns like a Triceratops. It wears a bandana.",
     quote: "“My champion!”",
     reasonwhyIlikechar: "He is such a baby, I love him.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=TUOQEHGrAg4",
+    songthatfitsthemtext: "First Riders",
     number: "8"
 }
 
 let hoshino = {
     picture: "pshoshino.png",
+    name: "Hachirota Hoshino",
     description: "Hachirota Hoshino (星野　八郎太 Hoshino Hachirōta), often called Hachimaki (鉢巻) or just Hachi (はち), is the protagonist of the anime as well as the manga. A twenty-five year old debris collector, Hachimaki starts out a little full of himself as he trains Ai Tanabe to perform EVA work, while his ego and hotheadedness are not exactly an asset he is able to provide her with a solid mentor figure in addition to a good understanding of EVA operations.",
     quote: "“Having something in life that is beyond doubt is a truly wonderful thing. Don't you agree, Kyutaro?”",
     reasonwhyIlikechar: "Hoshino as a main character is pretty interesting, someone who has been in the field of work with space junk teaching someone new is nice. Also his character development is pretty good.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=YOFP2SVcnLI",
+    songthatfitsthemtext: "Jupiter highway",
     number: "9"
 }
 
 let rodan = {
     picture: "gzrodan.png",
+    name: "Rodan",
     description: "Rodan (ラドン Radon) is a giant Pteranodon kaiju who first appeared in the 1956 Toho film Rodan, and made his first appearance alongside Godzilla in the 1964 Godzilla film Ghidorah, the Three-Headed Monster.",
     quote: "“The last of their kind, masters of the air and earth, the strongest, swiftest creatures that ever breathed... now they sank against the earth like weary children. Each had refused to live without the other, and so they were dying together. I wondered whether I, a 20th century man, could ever hope to die as well.”",
     reasonwhyIlikechar: "Rodan is so cool, he lives in a volcano, pretty much is a living fire creature.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=tqYqtvu-wSE",
+    songthatfitsthemtext: "The Fire Demon Suite",
     number: "10"
 }
 
 let revolverocelot = {
     picture: "mgsrevolverocelot.png",
+    name: "Revolver Ocelot",
     description: "Revolver Ocelot, real name Adamska (Russian: Адамска) or simply Adam (Russian: Адам), and also known as ADAM, Shalashaska (Russian: Шалашаска), or most commonly Ocelot (Russian: Оцелот), was a Russian-American operative of FOXHOUND and an agent of the Patriots.",
     quote: "“We need tension...conflict. The world today has become too soft. We're living in an age where true feelings are suppressed. So we're going to shake things up a bit. We'll create a world dripping with tension... ...a world filled with greed and suspicion, bravery and cowardice.”",
     reasonwhyIlikechar: "He made a whole meme showing how much sides he changed and I gotta say he got balls to do so.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=pPBHMgc9wVQ",
+    songthatfitsthemtext: "Way to Fall",
     number: "11"
 }
 
 let thevoice = {
     picture: "fithevoice.png",
+    name: "The Voice",
     description: "The Voice is a mysterious figure who claims to be an inmate in the prison. He frequently contacts The Stranger to tell him about the prison, the Guardians, and how to escape.",
     quote: "“The jailer is the key. Kill him, and you'll be free.”",
     reasonwhyIlikechar: "He is named the voice and he does have the voice really in both English and Japanese.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=gQ5gtInauiI",
+    songthatfitsthemtext: "You're mine",
     number: "12"
 }
 
 let mo = {
     picture: "mamo.png",
+    name: "MO",
     description: "MO is a little slime that the player controls.",
     quote: "“...”",
     reasonwhyIlikechar: "MO is cute also really fun to handle (sounds really wrong out of context) but I mean in a gameplay wise.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=Xc1BVmF2Ubc",
+    songthatfitsthemtext: "MO",
     number: "13"
 }
 
 let potemkin = {
     picture: "ggpotemkin.png",
+    name: "Potemkin",
     description: "Potemkin is a recurring character in the Guilty Gear series. He was a former slave in the Zepp Armed Air Empire, and was a supporter of the democratic revolution that took place. He is currently a member of Zepp's elite presidential guard, working closely with President Gabriel.",
     quote: "“What I do, I do for my comrades...”",
     reasonwhyIlikechar: "Potemkin has one of the quite saddest fate in the whole series, also he got some really cool moves.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=LcYNqLwkM8I",
+    songthatfitsthemtext: "Armord-clad Faith",
     number: "14"
 }
 
 let thedrifter = {
     picture: "hldthedrifter.png",
+    name: "The drifter",
     description: "The Drifter, referred to on his tarot card as 'The Magician', is the main playable character in Hyper Light Drifter. He is a member of the feline Blu species who has been infected with a strange terminal disease. The Drifter came to the Land of Light from the Northern Realms and travels the new continent in search of a cure.",
     quote: "“A lonely soul, in a desperate search.”",
     reasonwhyIlikechar: "He has a design that just looks really cool and his goal itself is pretty cool.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=MewrEPVruTM",
+    songthatfitsthemtext: "Vignette: Panacea",
     number: "15"
 }
 
 let viral = {
     picture: "grviral.png",
+    name: "Viral",
     description: "Viral (ヴィラル Viraru) is a shark-like Beastman with feline genetics, commander of the Human Eradication Army's Far East Theater. He is directly subordinate to General Thymilph, and after his death, he cooperates with Adiane and Cytomander in the hope of taking revenge on Kamina. Controls Enki's personal gunmen. He is also the archrival of Simon.",
     quote: "“You spout a bunch of crap, but this is crap I can really get behind!”",
     reasonwhyIlikechar: "His mech and his characteristics are badass, also him turning from a rival to a somewhat friend is pretty great.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=irPyQoL7UJg",
+    songthatfitsthemtext: "Nikopol",
     number: "16"
 }
 
 let radshiba = {
     picture: "v1radshiba.png",
+    name: "Rad Shiba",
     description: "Rad Shiba is a dog who 'works' part time at VA-11 Hall-A, although most of the time he's treated as a patron. ",
     quote: "“You'll pet me sooner or later. They all do.”",
     reasonwhyIlikechar: "I think it's self explanatory why I like him a lot. He. Is. A. Rad. Shiba.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=qmaTQSVGQ3s",
+    songthatfitsthemtext: "Digital Drive",
     number: "17"
 }
 
 let fifteen = {
     picture: "kzfifteen.png",
+    name: "Fifteen",
     description: "Fifteen, nicknamed 'The Dragon' by the media, is the deuteragonist of Katana Zero.  Over the course of the game, several mass-murders are attributed to him, and he clashes with Zero several times as he seeks revenge against the government for creating him.",
     quote: "“He is not the Dragon. I am. ”",
     reasonwhyIlikechar: "Cool design, cool powers, overall cool character.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=faZEYN4KHx0",
+    songthatfitsthemtext: "Breath of a Serpent",
     number: "18"
 }
 
 let glitch = {
     picture: "sbglitch.png",
+    name: "The Glitch",
     description: "The Glitch are one of the seven playable races in Starbound, and one of the six races entrusted with an Ancient artifact.",
     quote: "“Beep boop”",
     reasonwhyIlikechar: "These guys are robots and they live in a medieval era houses.",
+    songthatfitsthem: "https://www.youtube.com/watch?v=Z5EJLFu5XWQ",
+    songthatfitsthemtext: "Error 0xbfaf000",
     number: "19"
 }
 
